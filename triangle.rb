0b86c07bdd55dc426642c0401594a40e6deedf3c
Shannon <shannonskipper@gmail.com>
2011-08-23 15:11:43 -0500

    update

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,14 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  case [a, b, c].uniq.count
+  when 3
+    :equilateral
+  when 2
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
