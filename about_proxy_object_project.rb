664eafc2364e77ee14e6743b8606ae25483409a0
Havenwood <shannonskipper@gmail.com>
2011-08-28 20:17:06 -0500

    update

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,25 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
+  
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages = []
   end
-
-  # WRITE CODE HERE
+  
+  def method_missing(method, *stuff, &block)
+    @messages << method
+    @object.send(method, *stuff)
+  end
+  
+  def called?(message)
+    @messages.include?(message)
+  end
+  
+  def number_of_times_called(message)
+    @messages.count(message)
+  end
 end
 
 # The proxy object should pass the following Koan:
@@ -101,7 +114,8 @@ class Television
   attr_accessor :channel
   
   def power
-    if @power == :on
+    case @power
+    when :on
       @power = :off
     else
       @power = :on
